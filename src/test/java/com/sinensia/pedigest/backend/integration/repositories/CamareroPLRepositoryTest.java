bd38db82051ebedea79e750e2ae38535edfb5c43
Jordi <info@gmail.com>
2021-06-24 15:52:02 +0200

    se amplian los test de CamareroPLRepository

--- src/test/java/com/sinensia/pedigest/backend/integration/repositories/CamareroPLRepositoryTest.java
+++ src/test/java/com/sinensia/pedigest/backend/integration/repositories/CamareroPLRepositoryTest.java
@@ -1,8 +1,11 @@
 package com.sinensia.pedigest.backend.integration.repositories;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +28,34 @@ public class CamareroPLRepositoryTest {
 		CamareroPL createdCamareroPL = camareroPLRepository.save(camareroPL);
 		
 		assertEquals("Carlota Cifuentes", createdCamareroPL.getNombre());
+		assertNotEquals(0, createdCamareroPL.getId());
 	}
 	
 	@Test
 	public void should_retrieve_all_camarero() {
-		System.err.println("**************************** TEST! ***************************************");
+		
 		List<CamareroPL> camareros = camareroPLRepository.findAll();
 		
 		assertEquals(2, camareros.size());	
+	}
+	
+	@Test
+	public void should_return_total_camareros_count() {
+		
+		long numeroTotalCamareros = camareroPLRepository.count();
+		
+		assertEquals(3, numeroTotalCamareros);	
+	}
+	
+	@Test
+	public void should_return_camarero_by_id() {
+		
+		Optional<CamareroPL> optional = camareroPLRepository.findById(101);
+		
+		assertTrue(optional.isPresent());
+		CamareroPL camareroPL = optional.get();
+		assertEquals("Honorio Martín Salvador H2", camareroPL.getNombre());
+		
 	}
 	
 	
